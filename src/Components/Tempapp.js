b0c4c79550440763b108891f2cdee06e19330396
imnitingaur <70573363+imnitingaur@users.noreply.github.com>
2022-07-01 17:43:55 +0530

    Add files via upload

--- src/Components/Tempapp.js
+++ src/Components/Tempapp.js
@@ -3,42 +3,38 @@ import "./css/style.css";
 
 const Tempapp = () => {
     const [city, setCity] = useState(null);
-    const [search, setSearch] = useState("Dehradun");
-    const [searchHistory, setSearchHistory] = useState([]);
+    const [search, setSearch] = useState("Delhi");
 
+//     var istrue=false;
+//     const cities = ["Delhi","Dehradun","Kotdwara"];
+//    /* if(istrue==false)
+//     {
+//         setSearch(search.concat(cities))
+    
+//     }*/
+//     setSearch(search.concat(cities))
+//     console.log(cities)
     useEffect ( () => {
+        console.log("Inside effect")
         const fetchApi = async () => {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=7938d9005e68d8b258a109c716436c91`
+            console.log("API effect")
             const response = await fetch(url);  
             const resJson = await response.json();
             setCity(resJson.main);
         };
         fetchApi();
 
-       },[search] )
+    },[search] )
 
-        const handleClick = event => {
-        event.preventDefault();
-        searchHistory.push(search)
-        };
+
 
 return(
         <>
             <div className="box">
                 <div className="inputData">
-                <input
-                id="btnc"
-                type="search" 
-                value={search}
-                className="inputFeild" 
-                onChange={ (event) => { setSearch(event.target.value) }}
-               />
-               <button onClick={handleClick}>Click</button>
               </div>
              <input className="is"></input>
-             {searchHistory.map((user) => (
-            <h2 className="tempmin_max">{user}</h2>
-            ))}
             {!city ? (
             <p className="errorMsg">Enter City Name</p>
            ) : (
