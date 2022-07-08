fbc0a7595cf14b3dd58c7819ff33bc3786031494
imnitingaur <70573363+imnitingaur@users.noreply.github.com>
2022-07-08 18:56:23 +0530

    Update Tempapp.js

--- src/Components/Tempapp.js
+++ src/Components/Tempapp.js
@@ -2,7 +2,7 @@ import react,{useEffect, useState} from "react";
 import "./css/style.css";
 
 const Tempapp = () => {
-    const [city, setCity] = useState(null);
+    var [city, setCity] = useState([]);
     const [search, setSearch] = useState("");
     const [istru, setIstru] = useState(false);
     const [cities, setCities] = useState(["Delhi","Dehradun","Kotdwara","Pune"])
@@ -19,62 +19,44 @@ const Tempapp = () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=7938d9005e68d8b258a109c716436c91`;
         const response = await fetch(url);
         const resJson = await response.json();
-        
         setTpt({[search]:resJson.name});
-        const city = resJson.main;
+        const tptcity = resJson.main;
+        const namecity = resJson.name;
+        tptcity['name'] = namecity;
+         setCity(tptcity);
       
-        return city;
         };
       
     
       const getAllCities = async() => {
         cities.map(city => getCity(city))
+        
       }
+
     
       useEffect(() => {
         getAllCities();
-      }, [cities]);
-
+      }, [cities]); 
+    console.log(city)   
+  
 return(
         <>
-            <div className="box">
+       
+            <div className="box">  
                 <div className="inputData">
               </div>
              <input className="is"></input>
-            {!city ? (
+              {!city ? (
             <p className="errorMsg">Enter City Name</p>
-           ) : (
+           ) : (  
             <div>
-               
-                <div className="info">
-                <h2 className="location">
-                <i className="fa-solid fa-street-view"> </i>{search}
-                </h2>
-                <h1 className="temp">
-                {cities.temp}
-                </h1>
-                <h3 className="tempmin_max">
-                   Min : {cities.temp_min} | Max : {cities.temp_max}
-                </h3>
-                </div>
                 <div className="wave -one"></div>
                 <div className="wave -two"></div>
                 <div className="wave -three"></div>
-
-
                 <div className="info">
-                <h2 className="location">
-                <i className="fa-solid fa-street-view"> </i>{search}
-                </h2>
-                {tpt.map((city) => {
-                return  <h3 className="tempmin_max">
-                   Min : {city} | Max : {city}    
-                </h3>
-                })}
-                </div>
+                <h3 className="tempmin_max">Name : {city.name} | Temprature : {city.temp} </h3>
+                </div> 
             </div>
-
-            
             ) }
             </div>
         </>
